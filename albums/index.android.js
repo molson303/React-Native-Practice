8b2e12778036827e6db6959ef7d696be1e2cbe1e
MIchael <michael.olson756@gmail.com>
2016-09-28 16:05:01 -0600

    header created

--- albums/index.android.js
+++ albums/index.android.js
@@ -1,12 +1,16 @@
 //import a library to help create a Component
 import React from 'react';
-import {Text, AppRegistry} from 'react-native';
-
+import {AppRegistry, View} from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 //create a Component
 //A component is something that produces some amount of text or content that we can render on a mobile device
 //A JS function that returns some amount of JSX
 const App = () => (
-    <Text>Some Text</Text>
+<View>
+  <Header headerText={'Albums'}/>
+  <AlbumList />
+</View>
 );
 
 
